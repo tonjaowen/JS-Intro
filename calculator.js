d2d3263c8b5fc1da1fe9be0302d1957637cf72c8
Tonja Owen <tonjaowen@Tonjas-MacBook-Pro.local>
2018-05-03 16:50:38 -0700

    fixed

--- calculator.js
+++ calculator.js
@@ -1,26 +1,99 @@
 const operationType = prompt("Please enter the operation type, +, -, *, /")
-const firstOperand = prompt("Please enter first operand")
-const secondOperand = prompt("Please enter second operand")
-let result
-//perform operation
 
-if(operationType =="+") {
-    result = Number(firstOperand) + Number(secondOperand)
+const firstOperand = parseFloat(prompt("Please enter first operand"))
+const secondOperand = parseFloat(prompt("Please enter second operand"))
+let result
+
+//Addition result
+
+if(operationType ==  "+" ) {
+    result = (firstOperand + secondOperand)
 }  
 
-else if (operationType =! null){
+else if (operationType == ""){
     result = "Error"
 }
 
-else if (firstOperand = !null){
+else if (firstOperand == ""){
     result = "Error"
 }
 
-else if (secondOperand = !null){
+else if (secondOperand == ""){
     result = "Error"
 }
 
-else if (oper =="-"){
-    result = Number (firstOperand) - Number (secondOperand)
+let resultMessage = (` ${firstOperand} plus ${secondOperand} is ${result }`)
+
+console.log (resultMessage)
+
+
+//Subtraction result
+
+if(operationType ==  "-" ) {
+    result = (firstOperand - secondOperand)
+}  
+
+else if (operationType == ""){
+    result = "Error"
 }
-console.log ( `${firstOperand}  ${operandType} ${secondOperand} is `)
+
+else if (firstOperand == ""){
+    result = "Error"
+}
+
+else if (secondOperand == ""){
+    result = "Error"
+}
+
+
+let subtractResultMessage = (` ${firstOperand} subtracted by  ${secondOperand} is ${result }`)
+
+console.log (subtractResultMessage)
+
+//Multiplication result
+
+
+if(operationType ==  "*" ) {
+    result = (firstOperand * secondOperand)
+}  
+
+else if (operationType == ""){
+    result = "Error"
+}
+
+else if (firstOperand == ""){
+    result = "Error"
+}
+
+else if (secondOperand == ""){
+    result = "Error"
+}
+
+let multiplicationResultMessage = (` ${firstOperand} multiplied by ${secondOperand} is ${result }`)
+
+console.log (multiplicationResultMessage)
+
+
+//Division result
+
+if(operationType ==  "/" ) {
+    result = (firstOperand / secondOperand)
+}  
+
+else if (operationType == ""){
+    result = "Error"
+}
+
+else if (firstOperand == ""){
+    result = "Error"
+}
+
+else if (secondOperand == ""){
+    result = "Error"
+}
+let divided =  "/"
+let divisionResultMessage = (` ${firstOperand} divided by ${secondOperand} is ${result }`)
+
+console.log (divisionResultMessage)
+
+
