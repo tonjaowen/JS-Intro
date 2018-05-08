695b3fd80728d4277375d5449094d065c68c0715
Tonja Owen <tonjaowen@Tonjas-MacBook-Pro.local>
2018-05-08 13:18:01 -0700

    work

---
+++ shape_loop.js
@@ -0,0 +1,10 @@
+while (true) {
+    const shapeType = prompt("Please enter a shape: square, triangle, or q for quit.")
+
+    if (shapeType === "q") {
+        alert("thank you.")
+        break    
+    } else {
+        const size = prompt("Please enter a size: 1, 2, 3...")
+    }
+}
